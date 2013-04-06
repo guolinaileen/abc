04ba6e67e5896d4243732720dbf3eecab2c6509c
guolinaileen <guolinaileen@gmail.com>
2013-04-05 17:51:28 -0700

    Create Reverse Integer.java

---
+++ Reverse Integer.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int reverse(int x) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        boolean negative=false; 
+        if(x<0) 
+        {
+            negative=true; 
+            x=-x; 
+        }
+        int counter=1; 
+        while(x/counter!=0)
+        {
+            counter*=10; 
+        }
+        if(counter==10) return x; 
+        else counter/=10; 
+        int result=0; 
+        while(x!=0)
+        {
+            if(Integer.MAX_VALUE/counter<x%10) 
+            return negative? Integer.MIN_VALUE: Integer.MAX_VALUE; 
+            result+=x/counter+ (x%10 *counter);
+            x=(x%counter)/10;
+            counter/=10; 
+        }
+        return negative? -result: result; 
+    }
+}
