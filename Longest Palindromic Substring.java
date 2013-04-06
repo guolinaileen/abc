1a06b862ec85ecfe3692064c9d5f5ef95a11f5f1
guolinaileen <guolinaileen@gmail.com>
2013-04-05 17:13:15 -0700

    Create Longest Palindromic Substring.java

---
+++ Longest Palindromic Substring.java
@@ -0,0 +1,50 @@
+public class Solution {
+    public String longestPalindrome(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(s.length()==0) return s; 
+        String original=s; 
+        s=preProcess(s);
+        int c=0, l=0, r=0; 
+        int [] p=new int[s.length()];
+        for(int i=0; i<s.length(); i++)
+        {
+            int i_prime=c-(i-c); 
+            if(r>i && i_prime>=0)
+            {
+                if(p[i_prime]<=r-i) p[i]=p[i_prime];
+            }
+            while(i-p[i]-1>=0 && i+p[i]+1<s.length() &&  s.charAt(i-p[i]-1)==s.charAt(i+p[i]+1))
+            {
+                p[i]++; 
+            }
+            if(p[i]+i>r)
+            {
+                c=i;
+                r=p[i]+i;
+            }
+        }
+        int maxL=0, center=0; 
+        for(int i=0; i<s.length(); i++)
+        {
+            if(p[i]>maxL)
+            {
+                center=i; 
+                maxL=p[i]; 
+            }
+        }
+        return original.substring((center-maxL)/2, (center+maxL)/2);
+    }
+    String preProcess(String s)
+    {
+        StringBuffer sb=new StringBuffer();
+        for(int i=0; i<s.length(); i++)
+        {
+            sb.append("#");
+            sb.append(s.charAt(i));
+        }
+        sb.append("#");
+        return sb.toString(); 
+    }
+    
+}
