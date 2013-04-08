341ce813d5e6ea56df4364cc6f59c19d322729e4
guolinaileen <guolinaileen@gmail.com>
2013-04-07 20:01:05 -0700

    Create Recover Binary Search Tree.java
    haven't been tested

---
+++ Recover Binary Search Tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    TreeNode n1; 
+    TreeNode n2; 
+    TreeNode pre; 
+    public void recoverTree(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        n1=null; n2=null; pre=null; 
+        inOrder(root);
+        if(n1!=null && n2!=null)
+        {
+         int temp=n1.val;
+         n1.val=n2.val; 
+         n2.val=temp; 
+        }
+    }
+    void inOrder(TreeNode root)
+    {
+        if(root==null) return ; 
+        inOrder(root.left);
+        if(pre!=null && pre.val>root.val)
+        {
+            n2=root; 
+            if(n1==null)
+            {
+                n1=pre; 
+            }
+        }
+        pre=root; 
+        inOrder(root.right);
+    }
+    
+}
