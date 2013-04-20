83f4d95f70fbd5db5a68edd3a2e13dc40af43f87
guolinaileen <guolinaileen@gmail.com>
2013-04-20 15:58:28 -0700

    Create Subsets II.java

---
+++ Subsets II.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+         ArrayList<ArrayList<Integer>> result=new  ArrayList<ArrayList<Integer>>(); 
+         ArrayList<Integer> list=new ArrayList<Integer>();
+         result.add(list);
+         Arrays.sort(num);
+         int start=0; 
+         for(int i=0; i<num.length; i++)
+         {
+             int size=result.size();
+             for(; start<size; start++)
+             {
+                 ArrayList<Integer> subList=new ArrayList(result.get(start));
+                 subList.add(num[i]);
+                 result.add(subList);
+             }
+             if(i+1<num.length && num[i]==num[i+1])
+             {
+                 start=size;
+             }else
+             {
+                 start=0; 
+             }
+         }
+         return result; 
+    }
+}
