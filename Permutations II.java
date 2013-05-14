1bc569533a641ed86021a9ed394c7b6ca94330d4
guolinaileen <guolinaileen@gmail.com>
2013-05-14 16:03:03 -0600

    Create Permutations II.java

---
+++ Permutations II.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>(); 
+        if(num.length==0) return result; 
+        Arrays.sort(num);
+        boolean [] visited=new boolean[num.length];
+        ArrayList<ArrayList<Integer>> list=new ArrayList<ArrayList<Integer>>(); 
+        ArrayList<Integer> sublist=new ArrayList<Integer>(); 
+        permute(num, visited, sublist, list); 
+        return list; 
+    }
+    void permute(int [] num, boolean [] visited, ArrayList<Integer> sublist, ArrayList<ArrayList<Integer>> list)
+    {
+        if(sublist.size()==num.length)
+        {
+            list.add(new ArrayList<Integer>(sublist)); return ;
+        }
+        for(int i=0; i<num.length; i++)
+        {
+            if(i!=0 && num[i]==num[i-1] && visited[i-1]) continue; 
+            if(!visited[i])
+            {
+                sublist.add(num[i]);
+                visited[i]=true; 
+                permute(num, visited, sublist, list);
+                visited[i]=false; 
+                sublist.remove(sublist.size()-1);
+            }
+        }
+        
+    }
+}
