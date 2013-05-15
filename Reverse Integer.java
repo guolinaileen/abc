c79969e830c3038e60cb3de4f1ff22f45fbe9848
guolinaileen <guolinaileen@gmail.com>
2013-05-15 15:19:51 -0600

    Update Reverse Integer.java

--- Reverse Integer.java
+++ Reverse Integer.java
@@ -2,20 +2,15 @@ public class Solution {
     public int reverse(int x) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        boolean negative=false; 
-        if(x<0) 
-        {
-            negative=true; 
-            x=-x; 
-        }
-        int result=0; 
+        boolean negative=x<0? true: false; 
+        if(negative)x=-x;
+        int num=0; 
         while(x!=0)
         {
-            if(Integer.MAX_VALUE/10<result) 
-             return negative? Integer.MIN_VALUE: Integer.MAX_VALUE; 
-            result=result*10+ x%10;
-            x/=10; 
+            if((Integer.MAX_VALUE-x%10)/10<num) return negative? Integer.MIN_VALUE: Integer.MAX_VALUE; 
+            num=10*num+x%10; 
+            x=x/10; 
         }
-        return negative? -result: result; 
+        return negative? -num: num; 
     }
 }
