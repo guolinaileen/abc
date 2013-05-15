a2b291e23ded9eb8c7b91c9e49da57ab52672303
guolinaileen <guolinaileen@gmail.com>
2013-05-15 15:40:11 -0600

    Update String to Integer (atoi).java

--- String to Integer (atoi).java
+++ String to Integer (atoi).java
@@ -2,30 +2,26 @@ public class Solution {
     public int atoi(String str) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        int length=str.length();
+        if(length==0) return 0; 
+        int index=0; 
+        while(index<length && str.charAt(index)==' ')
+        {
+            index++; 
+        }
         boolean negative=false; 
-        int result=0; 
-        int start=0; 
-        while(start<str.length() && str.charAt(start)==' ')
+        if(str.charAt(index)=='-'||str.charAt(index)=='+' )
         {
-            start++; 
+            negative=str.charAt(index)=='-'? true: false;
+            index++; 
         }
-        for(int i=start; i<str.length(); i++)
+        int num=0; 
+        for(int i=index; i<length; i++)
         {
-            if(i==start)
-            {
-                if(str.charAt(i)=='-')
-                {
-                    negative=true; continue;
-                }
-                if(str.charAt(i)=='+') continue; 
-            }
-            if(str.charAt(i)<='9' && str.charAt(i)>='0')
-            {
-                if((Integer.MAX_VALUE-str.charAt(i)+'0')/10<result)
-                return negative? Integer.MIN_VALUE: Integer.MAX_VALUE; 
-                result=result*10+str.charAt(i)-'0';
-            }else break; 
+            if(!(str.charAt(i)<='9'&&str.charAt(i)>='0')) break; 
+            if((Integer.MAX_VALUE-(str.charAt(i)-'0'))/10<num) return negative? Integer.MIN_VALUE: Integer.MAX_VALUE;
+            num=num*10+(str.charAt(i)-'0');
         }
-        return negative? -result: result; 
+        return negative? -num: num; 
     }
 }
