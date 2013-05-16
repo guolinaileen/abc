31b23a21f29477c3e1ecd428d4728fbf5f8f2287
guolinaileen <guolinaileen@gmail.com>
2013-05-16 00:53:46 -0600

    Update Longest Valid Parentheses.java

--- Longest Valid Parentheses.java
+++ Longest Valid Parentheses.java
@@ -1,45 +1,34 @@
 public class Solution {
-    class Pair
-    {
-        char c; 
-        int index; 
-        Pair(char val, int i)
-        {
-            c=val; 
-            index=i; 
-        }
-    }
     public int longestValidParentheses(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if(s.length()<2) return 0;
-        Stack<Pair> stack=new Stack<Pair>(); 
-        int max=0; 
-        for(int i=0; i<s.length(); i++)
-        {
-            if(s.charAt(i)=='(')
-            {
-                stack.push(new Pair('(', i));
-            }else
-            {
-                
-                if(stack.isEmpty() || stack.peek().c==')') 
-                {
-                    stack.push(new Pair(')', i));
-                }else
-                {
-                    stack.pop(); 
-                    if(stack.isEmpty())
-                    {
-                        max=Math.max(max, i+1);
-                    }else
-                    {
-                        Pair peek=stack.peek();
-                        max=Math.max(max, i-peek.index);
-                    }
-                }
-            }
-        }
-        return max; 
+       if(s.length()==0) return 0; 
+       Stack<Integer> stack=new Stack<Integer>(); 
+       int left=0; 
+       int max=0; 
+       for(int i=0; i<s.length(); i++)
+       {
+           if(s.charAt(i)=='(')
+           {
+               stack.push(i);
+           }else
+           {
+               if(stack.isEmpty())
+               {
+                   left=i+1; 
+               }else
+               {
+                   stack.pop(); 
+                   if(stack.isEmpty())
+                   {
+                       max=Math.max(max, i-left+1);
+                   }else
+                   {
+                       max=Math.max(max, i-stack.peek());
+                   }
+               }
+           }
+       }
+       return max; 
     }
 }
