0e456755e7bfa8f92a5914913fcef8dda73da75b
guolinaileen <guolinaileen@gmail.com>
2013-05-16 14:03:55 -0600

    Update Jump Game.java

--- Jump Game.java
+++ Jump Game.java
@@ -2,21 +2,22 @@ public class Solution {
     public boolean canJump(int[] A) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int length=A.length;
-        if(length==0) return true; 
-        for(int i=0; i<length; )
-        {
-           int counter=A[i];
-           if(i+counter>=length-1) return true;
-           while(counter!=0)
-           {           
-              if(A[i+counter]==0) counter--; 
-              else break; 
-           }
-           if(counter==0) return false; 
-           i=i+counter; 
-        }
-        return false; 
+      if(A.length==0) return false;   
+      for(int i=0; i<A.length; )
+      {
+          int step=A[i];
+          if(i+step>=A.length-1) return true; 
+          int j=step; 
+          for(; j>0; j--)
+          {
+              if(A[i+j]!=0)
+              {
+                  i=i+j; 
+                  break; 
+              }
+          }
+          if(j==0) return false; 
+      }
+      return true; 
     }
-
 }
