6ec7cb0f58da69af5ec212f8fcf5a457c550bd1d
guolinaileen <guolinaileen@gmail.com>
2013-05-15 22:19:24 -0600

    Update Implement strStr().java

--- Implement strStr().java
+++ Implement strStr().java
@@ -2,21 +2,12 @@ public class Solution {
     public String strStr(String haystack, String needle) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        if(haystack.length()<needle.length()) return null;
         if(needle.length()==0) return haystack; 
-        if(haystack.length()==0) return null; 
         for(int i=0; i<=haystack.length()-needle.length(); i++)
         {
-            if(haystack.charAt(i)==needle.charAt(0))
-            {
-                for(int j=0; j<=needle.length(); j++ )
-                {
-                    if(j==needle.length()) return haystack.substring(i);
-                    if(haystack.charAt(i+j)!=needle.charAt(j))
-                    {
-                        break; 
-                    }
-                }   
-            }
+            String temp=haystack.substring(i, i+needle.length());
+            if(temp.equals(needle)) return haystack.substring(i);
         }
         return null; 
     }
