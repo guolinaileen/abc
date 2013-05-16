8fefe20ac47e51f382fba8c52d2140a181b6723f
guolinaileen <guolinaileen@gmail.com>
2013-05-16 11:54:10 -0600

    Update Combination Sum II.java

--- Combination Sum II.java
+++ Combination Sum II.java
@@ -2,25 +2,26 @@ public class Solution {
     public ArrayList<ArrayList<Integer>> combinationSum2(int[] num, int target) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        ArrayList<Integer> list=new ArrayList<Integer>(); 
         ArrayList<ArrayList<Integer>> result=new ArrayList<ArrayList<Integer>>(); 
-        ArrayList<Integer> subResult=new ArrayList<Integer>();
         Arrays.sort(num);
-        solve(result, num, 0, target, -1,  subResult );  
-        return result;
+        combination(num, 0, target, 0, list, result ) ;
+        return result; 
     }
-    void solve(ArrayList<ArrayList<Integer>> result, int[] num, int sum, int target, int index, ArrayList<Integer> subResult )
+    void combination(int [] candidates, int index,int target, int sum, ArrayList<Integer> list, ArrayList<ArrayList<Integer>> result )
     {
         if(sum>target) return ;
         if(sum==target)
         {
-            result.add(new ArrayList<Integer>( subResult ));
+            result.add(new ArrayList<Integer>(list)); return ;
         }
-        for(int i=index+1; i<num.length; i++)
+        
+        for(int i=index; i<candidates.length; i++)
         {
-            if(i!=index+1 && num[i]==num[i-1]) continue; 
-            subResult.add(num[i]);
-            solve(result, num, sum+num[i], target, i, subResult );
-            subResult.remove(subResult.size()-1);
+            if(i>index && candidates[i]==candidates[i-1]) continue; 
+            list.add(candidates[i]);
+            combination(candidates, i+1, target ,sum+candidates[i], list, result);
+            list.remove(list.size()-1);
         }
     }
 }
