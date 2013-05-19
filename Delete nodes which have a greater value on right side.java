2c285b21ab49ea04988aaa59e2846241ad84dcce
guolinaileen <guolinaileen@gmail.com>
2013-05-18 19:10:49 -0600

    Update Delete nodes which have a greater value on right side.java

--- Delete nodes which have a greater value on right side.java
+++ Delete nodes which have a greater value on right side.java
@@ -7,8 +7,8 @@ public static Node deleteNodes(Node head, Node pre)
 		if(tail==null) return head; 
 		if(head.val<tail.val) 
 		{
-			if(pre!=null) pre.next=tail; 
+			if(pre!=null) pre.next=tail;  //delete head because the value of head is smaller than tail.
 			return tail; 
-			}
+		}
 		return head; 
 	}
