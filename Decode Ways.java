5aaf0ce0005af15a43fe38d9464f017fa3996d24
guolinaileen <guolinaileen@gmail.com>
2013-05-18 19:02:34 -0600

    Update Decode Ways.java

--- Decode Ways.java
+++ Decode Ways.java
@@ -8,10 +8,9 @@ public class Solution {
         for(int i=1; i<=s.length(); i++)
         {
             int c1=0; 
-            if(s.charAt(i-1)!='0')
-            c1=P[i-1];
+            if(s.charAt(i-1)!='0') c1=P[i-1]; //1 digit
             int c2=0; 
-            if(i-2>=0 && (s.charAt(i-2)=='1'|| (s.charAt(i-2)=='2' &&s.charAt(i-1)<='6')))
+            if(i-2>=0 && (s.charAt(i-2)=='1'|| (s.charAt(i-2)=='2' &&s.charAt(i-1)<='6'))) //2 digits
             c2=P[i-2];
             P[i]=c1+c2;
         }
