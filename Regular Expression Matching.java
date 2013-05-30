318af595ce30a14b9db405e0e3e67ee5a58049fb
guolinaileen <guolinaileen@gmail.com>
2013-05-29 23:55:11 -0600

    Update Regular Expression Matching.java

--- Regular Expression Matching.java
+++ Regular Expression Matching.java
@@ -19,3 +19,29 @@ public class Solution {
 	        return isMatch(s.substring(startS), p.substring(startP+2)); 
     }
 }
+
+
+/*
+another solution
+*/
+
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(p.length()==0 ) return s.length()==0; 
+        if(p.length()==1) return s.equals(p) || (p.equals(".")&&s.length()==1);
+        if(p.charAt(1)!='*')
+        {
+            if(s.length()!=0 &&(s.charAt(0)==p.charAt(0) || p.charAt(0)=='.'))
+            return isMatch(s.substring(1), p.substring(1));
+            else return false; 
+        }
+        if(isMatch(s, p.substring(2))) return true; 
+        if(s.length()>0 && (s.charAt(0)==p.charAt(0) || p.charAt(0)=='.'))
+        {
+            return isMatch(s.substring(1), p);
+        }
+        return false; 
+    }
+}
