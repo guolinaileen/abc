d11b9d7ec607912851014915e7428aa652f39a5c
guolinaileen <guolinaileen@gmail.com>
2013-05-29 19:54:52 -0600

    Update Palindrome Number.java

--- Palindrome Number.java
+++ Palindrome Number.java
@@ -19,6 +19,7 @@ public class Solution {
         }
         d/=10; 
         */
+
         while(x!=0)
         {
             int left=x/length;
@@ -26,6 +27,29 @@ public class Solution {
             if(left!=right) return false; 
             x=(x-left*length)/10; 
             length/=100; 
+        }
+        return true; 
+    }
+}
+
+        /*
+        it can be done in this way: 
+        */
+public class Solution {
+    public boolean isPalindrome(int x) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(x<0) return false; 
+        int d=1;
+        while(x/d>=10)
+        {
+            d*=10;
+        }
+        while(d!=0)
+        {
+            if(x/d!=x%10) return false; 
+            x=(x-x/d*d)/10; 
+            d/=100;
         }
         return true; 
     }
