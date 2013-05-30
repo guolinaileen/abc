0194ecf270fdabfd872dd57e93f4bfd90deeae8e
guolinaileen <guolinaileen@gmail.com>
2013-05-29 19:30:19 -0600

    Update ZigZag Conversion.java

--- ZigZag Conversion.java
+++ ZigZag Conversion.java
@@ -2,27 +2,20 @@ public class Solution {
     public String convert(String s, int nRows) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if(nRows==1) return s; 
-        int nodes=2*(nRows-1); //nRows+nRows-2 
-        int zones=s.length()%nodes==0? s.length()/nodes: (s.length()/nodes+1) ;
-        StringBuffer sb=new StringBuffer(); 
-        for(int i=0; i<nRows; i++)
-        {
-            for(int j=0; j<zones; j++)
-            {
-                if(i+nodes*j<s.length())
-                {
-                    sb.append(s.charAt(i+nodes*j));
-                }    
-                if(i!=0 && i!=nRows-1)
-                {
-                 if(nodes*(j+1)-i<s.length())
-                 {
-                 sb.append(s.charAt(nodes*(j+1)-i));
-                 }    
-                }
-            }
-        }
-        return sb.toString(); 
+      if(s.length()==0 || nRows==0 || nRows==1) return s; 
+      int words=2*(nRows-1); 
+      int region=s.length()%words==0? s.length()/words: (s.length()/words+1);
+      StringBuffer sb=new StringBuffer(); 
+      for(int i=0; i<nRows; i++)
+      {
+          for(int j=0; j<region; j++)
+          {
+              if(i+j*words<s.length())
+                    sb.append(s.charAt(i+j*words));
+              if(i!=0 && i!=nRows-1 && (j+1)*words-i<s.length() )
+                    sb.append(s.charAt((j+1)*words-i));  
+          }
+      }
+      return sb.toString(); 
     }
 }
