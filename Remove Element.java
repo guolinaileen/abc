b9c8efa1e2d46cef96d1191d1df5e807f963f160
guolinaileen <guolinaileen@gmail.com>
2013-05-30 01:12:23 -0600

    Update Remove Element.java

--- Remove Element.java
+++ Remove Element.java
@@ -17,3 +17,26 @@ public class Solution {
         return length; 
     }
 }
+
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(A.length==0) return 0; 
+        int index=0; 
+        int end=A.length-1;
+        while(index<=end)
+        {
+            if(A[index]==elem)
+            {
+                A[index]=A[end]; 
+                end--; 
+            }else
+            {
+                index++; 
+            }
+        }
+        if(end!=A.length-1) A[end+1]='\0'; 
+        return end+1; 
+    }
+}
