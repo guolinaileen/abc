a980af0693692a890414522dcc3df867e78ff98c
guolinaileen <guolinaileen@gmail.com>
2013-05-30 19:37:36 -0600

    Update Jump Game II_2.java

--- Jump Game II_2.java
+++ Jump Game II_2.java
@@ -24,3 +24,35 @@ public class Solution {
     }
 
 }
+
+/*
+anotehr solution
+*/
+
+public class Solution {
+    public int jump(int[] A) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(A.length==0||A.length==1) return 0;
+        int max=0; 
+        int index=0;
+        int count=0;
+        while(index<A.length)
+        {
+            int tempMax=0;
+            for(int i=index; i<=max; i++)
+            {
+                tempMax=Math.max(tempMax, i+A[i]);
+            }
+            if(tempMax>=A.length-1) return count+1; 
+            if(tempMax<max)
+            {
+                return -1;
+            }
+            index=max+1; 
+            max=tempMax; 
+            count++; 
+        }
+        return -1; 
+    }
+}
