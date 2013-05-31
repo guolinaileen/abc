a832f63341ade94a321c7a205e0294a0c3800afc
guolinaileen <guolinaileen@gmail.com>
2013-05-30 21:15:56 -0600

    Update Jump Game.java

--- Jump Game.java
+++ Jump Game.java
@@ -2,22 +2,20 @@ public class Solution {
     public boolean canJump(int[] A) {
         // Start typing your Java solution below
         // DO NOT write main() function
-      if(A.length==0) return false;   
-      for(int i=0; i<A.length; )
-      {
-          int step=A[i];
-          if(i+step>=A.length-1) return true; 
-          int j=step; 
-          for(; j>0; j--)
-          {
-              if(A[i+j]!=0)
-              {
-                  i=i+j; 
-                  break; 
-              }
-          }
-          if(j==0) return false; 
-      }
-      return true; 
+        int max=0; 
+        int index=0;
+        int start=0; 
+        while(max<A.length-1)
+        {
+            int tempMax=0; 
+            for(int i=start; i<=max; i++)
+            {
+                tempMax=Math.max(tempMax, A[i]+i);
+            }
+            if(tempMax<=max) return false; 
+            start=max+1; 
+            max=tempMax; 
+        }
+        return true; 
     }
 }
