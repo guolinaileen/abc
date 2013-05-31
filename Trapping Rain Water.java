321cbad12efa188838634d49b7da8421545bc88e
guolinaileen <guolinaileen@gmail.com>
2013-05-30 19:14:56 -0600

    Update Trapping Rain Water.java

--- Trapping Rain Water.java
+++ Trapping Rain Water.java
@@ -2,30 +2,30 @@ public class Solution {
     public int trap(int[] A) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if(A.length<=1) return 0; 
-        int []left=new int [A.length];
-        left[0]=A[0];
-        for(int i=1; i<A.length; i++)
-        {
-            if(left[i-1]>A[i])
-            left[i]=left[i-1];
-            else
-            left[i]=A[i]; 
-        }
-        int [] right=new int [A.length];
-        right[A.length-1]=A[A.length-1];
-        for(int i=A.length-2; i>=0; i--)
-        {
-            if(A[i]<right[i+1])
-            right[i]=right[i+1]; 
-            else
-            right[i]=A[i];
-        }
-        int result=0; 
-        for(int i=0; i<A.length; i++)
-        {
-            result+=Math.min(left[i], right[i])-A[i];
-        }
-        return result; 
+      if(A.length<=2) return 0; 
+      int [] first=new int[A.length];
+      int [] second=new int[A.length];
+      for(int i=0; i<A.length; i++)
+      {
+          if(i==0)
+          {
+          first[i]=A[i]; continue; 
+          }
+          first[i]=Math.max(first[i-1], A[i] );
+      }
+      for(int i=A.length-1; i>=0; i--)
+      {
+          if(i==A.length-1)
+          {
+          second[i]=A[i]; continue; 
+          } 
+          second[i]=Math.max(second[i+1], A[i] );
+      }
+      int max=0; 
+      for(int i=0; i<A.length; i++)
+      {
+          max+=Math.min(first[i], second[i])-A[i];
+      }
+      return max;
     }
 }
