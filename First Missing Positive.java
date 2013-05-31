0013028fd3f9c341b90a6ad81e31b813726513d6
guolinaileen <guolinaileen@gmail.com>
2013-05-30 19:05:58 -0600

    Update First Missing Positive.java

--- First Missing Positive.java
+++ First Missing Positive.java
@@ -2,27 +2,28 @@ public class Solution {
     public int firstMissingPositive(int[] A) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int length=A.length+1;
+        if(A.length==0) return 1; 
         for(int i=0; i<A.length; i++)
         {
-            if(A[i]<=0)
-            {
-                A[i]=length; 
-            }
+            if(A[i]<=0) A[i]=A.length+1; 
         }
         for(int i=0; i<A.length; i++)
         {
-            if(A[i]>=length||A[i]<0||A[A[i]-1]==-A[i]) continue; 
-            int temp=A[i];
-            A[i]=A[temp-1]; 
-            A[temp-1]=-temp; 
+            int num=Math.abs(A[i]);
+            if(num>A.length || A[num-1]==-num) continue;
+            if(A[num-1]==num)
+            {
+                A[num-1]=-num;  continue; 
+            }
+            int a=A[num-1]; 
+            A[num-1]=-A[i]; 
+            A[i]=a; 
             i--; 
         }
         for(int i=0; i<A.length; i++)
         {
-            if(A[i]!=-(i+1))
-            return i+1; 
+            if(A[i]>0) return i+1; 
         }
-        return length;
+        return A.length+1; 
     }
 }
