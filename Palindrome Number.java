ae44a6f03164367481e3bd3f9606ddc8098dc483
guolinaileen <guolinaileen@gmail.com>
2013-06-16 15:29:47 -0600

    Update Palindrome Number.java

--- Palindrome Number.java
+++ Palindrome Number.java
@@ -54,3 +54,26 @@ public class Solution {
         return true; 
     }
 }
+
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(x<0) return false; 
+        int length=1;
+        int Xt=x; 
+        while(Xt/10!=0)
+        {
+            length*=10; Xt=Xt/10; 
+        }
+        while(length>0)
+        {
+            if(x/length!=x%10) return false; 
+            x=x-(x/length*length);
+            x=x/10; 
+            length=length/100; 
+        }
+        return true; 
+    }
+}
